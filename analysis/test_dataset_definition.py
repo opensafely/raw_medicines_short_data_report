51c7846843e978459eb77ab5fb6a330cc74f9b0d
Em Prestige <69582748+emprestige@users.noreply.github.com>
2026-03-27 09:56:56 +0000

    add additional variables to checks

--- analysis/test_dataset_definition.py
+++ analysis/test_dataset_definition.py
@@ -29,42 +29,50 @@ test_data = {
             {
                 "date": date(2025, 2, 1),
                 "dmd_code": "10983311000001107",
-                "repeat_medication_id": None
+                "repeat_medication_id": None,
+                "medication_status": 0
             },
             {
                 "date": date(2025, 4, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 3
+                "repeat_medication_id": 3,
+                "medication_status": 0
             },
             {
                 "date": date(2025, 5, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 3
+                "repeat_medication_id": 3,
+                "medication_status": 0
             },
             {
                 "date": date(2025, 6, 6),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0
             },
             {
                 "date": date(2025, 8, 12),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0
             },
             {
                 "date": date(2025, 10, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0
             },
             {
                 "date": date(2025, 12, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0
             },
             {
                 "date": date(2026, 1, 15),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0
             }
         ],
         "repeat_medications_raw": [
@@ -72,54 +80,127 @@ test_data = {
                 "date": date(2025, 4, 1),
                 "start_date": date(2025, 4, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 3
+                "repeat_medication_id": 3,
+                "medication_status": 0,
+                "end_date": date(2026, 3, 5)
             },
             {
                 "date": date(2025, 5, 1),
                 "start_date": date(2025, 5, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 3
+                "repeat_medication_id": 3,
+                "medication_status": 0,
+                "end_date": date(2026, 8, 5)
             },
             {
                 "date": date(2025, 6, 6),
                 "start_date": date(2025, 6, 6),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0,
+                "end_date": date(2025, 12, 5)
             },
             {
                 "date": date(2025, 8, 12),
                 "start_date": date(2025, 9, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0,
+                "end_date": date(2025, 11, 1)
             },
             {
                 "date": date(2025, 10, 1),
                 "start_date": date(2025, 10, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0,
+                "end_date": date(2026, 1, 1)
             },
             {
                 "date": date(2025, 12, 1),
                 "start_date": date(2025, 12, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0,
+                "end_date": date(2026, 8, 1)
             },
             {
                 "date": date(2026, 1, 15),
                 "start_date": date(2026, 2, 1),
                 "dmd_code" : "3214311000001108",
-                "repeat_medication_id": 8
+                "repeat_medication_id": 8,
+                "medication_status": 0,
+                "end_date": date(2026, 2, 5)
             }
         ],
         "expected_in_population": True,
         "expected_columns": {
+            "active_repeats": 2,
             "meds_row_no": 8,
             "meds_rep_id_no": 2,
             "repeats_row_no": 7,
             "repeats_rep_id_no": 2,
             "rep_ids_differ": False,
             "concordant_dates": 5,
-            "discordant_dates": 2
+            "discordant_dates": 2,
+            "medications_status_0": 8,
+            "medications_status_1": 0,
+            "medications_status_2": 0,
+            "medications_status_3": 0,
+            "medications_status_4": 0,
+            "medications_status_5": 0,
+            "medications_status_6": 0,
+            "medications_status_7": 0,
+            "medications_status_8": 0,
+            "medications_status_9": 0,
+            "medications_status_10": 0,
+            "medications_status_11": 0,
+            "medications_status_12": 0,
+            "medications_status_13": 0,
+            "medications_status_14": 0,
+            "medications_status_15": 0,
+            "medications_status_16": 0,
+            "medications_status_17": 0,
+            "medications_status_18": 0,
+            "medications_status_19": 0,
+            "medications_status_20": 0,
+            "medications_status_21": 0,
+            "medications_status_22": 0,
+            "medications_status_23": 0,
+            "medications_status_24": 0,
+            "medications_status_25": 0,
+            "medications_status_26": 0,
+            "medications_status_27": 0,
+            "medications_status_28": 0,
+            "repeats_status_0": 7,
+            "repeats_status_1": 0,
+            "repeats_status_2": 0,
+            "repeats_status_3": 0,
+            "repeats_status_4": 0,
+            "repeats_status_5": 0,
+            "repeats_status_6": 0,
+            "repeats_status_7": 0,
+            "repeats_status_8": 0,
+            "repeats_status_9": 0,
+            "repeats_status_10": 0,
+            "repeats_status_11": 0,
+            "repeats_status_12": 0,
+            "repeats_status_13": 0,
+            "repeats_status_14": 0,
+            "repeats_status_15": 0,
+            "repeats_status_16": 0,
+            "repeats_status_17": 0,
+            "repeats_status_18": 0,
+            "repeats_status_19": 0,
+            "repeats_status_20": 0,
+            "repeats_status_21": 0,
+            "repeats_status_22": 0,
+            "repeats_status_23": 0,
+            "repeats_status_24": 0,
+            "repeats_status_25": 0,
+            "repeats_status_26": 0,
+            "repeats_status_27": 0,
+            "repeats_status_28": 0
         }
     },
 
