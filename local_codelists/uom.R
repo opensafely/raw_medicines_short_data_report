2ac482734802701aebffe22258d393f79d59733a
Em Prestige <emprest97@gmail.com>
2026-06-12 16:52:21 +0100

    add unit of measure to codelists

---
+++ local_codelists/uom.R
@@ -0,0 +1,114 @@
+library(opr)
+library(DBI)
+library(readr)
+library(bit64)
+
+# connect to bigquery DMD dataset
+con <- connect_bq(dataset = "dmd", credentials_var = "OP_CREDENTIALS")
+
+# create function to read codelist with correct column types
+read_codelist <- function(path) {
+  read_csv(
+    path,
+    col_types = cols(
+      code = col_character(),
+      term = col_character(),
+      dmd_id = col_character(),
+      dmd_type = col_character(),
+      bnf_code = col_character()
+    ),
+    show_col_types = FALSE
+  )
+}
+
+# convert dmd covdes to bigint for SQL queries
+as_int64 <- function(codes) {
+  as.integer64(codes[!is.na(codes) & codes != ""])
+}
+
+# convert VMP previous to codes to lookup_id for SQL queries
+vmp_lookup_id <- function(term, code) {
+  ifelse(
+    grepl("VMP previous to [0-9]+", term),
+    sub(".*VMP previous to ([0-9]+).*", "\\1", term),
+    code
+  )
+}
+
+# convert vector of codes to SQL IN clause
+in_clause <- function(codes) {
+  paste(as.character(as_int64(codes)), collapse = ",")
+}
+
+# build SQL query to fetch uom for VMPs
+build_vmp_uom_sql <- function(ids) {
+  paste0(
+    "SELECT\n",
+    "  CAST(vmp.id AS STRING) AS lookup_id,\n",
+    "  uom.descr AS uom\n",
+    "FROM `ebmdatalab.dmd.vmp` vmp\n",
+    "LEFT JOIN `dmd.unitofmeasure` uom ON vmp.udfs_uom = uom.cd\n",
+    "WHERE vmp.id IN (",
+    in_clause(ids),
+    ")\n"
+  )
+}
+
+# build SQL query to fetch uom for AMPs
+build_amp_uom_sql <- function(ids) {
+  paste0(
+    "SELECT\n",
+    "  CAST(amp.id AS STRING) AS lookup_id,\n",
+    "  uom.descr AS uom\n",
+    "FROM `ebmdatalab.dmd.amp` amp\n",
+    "LEFT JOIN `ebmdatalab.dmd.vmp` vmp ON amp.vmp = vmp.id\n",
+    "LEFT JOIN `dmd.unitofmeasure` uom ON vmp.udfs_uom = uom.cd\n",
+    "WHERE amp.id IN (",
+    in_clause(ids),
+    ")\n"
+  )
+}
+
+# fetch uom for given ids using provided SQL builder function
+fetch_uom <- function(ids, build_sql) {
+  ids <- unique(ids[!is.na(ids) & ids != ""])
+  if (length(ids) == 0) {
+    return(data.frame(lookup_id = character(), uom = character()))
+  }
+  dbGetQuery(con, build_sql(ids))
+}
+
+# main function to read codelist, fetch uom, and save updated codelist with uom column
+save_codelist <- function(output_path, input_path) {
+  cl <- read_codelist(input_path)
+  cl$lookup_id <- vmp_lookup_id(cl$term, cl$code)
+
+  vmp_uom <- fetch_uom(
+    cl$lookup_id[cl$dmd_type == "VMP"],
+    build_vmp_uom_sql
+  )
+  amp_uom <- fetch_uom(
+    cl$code[cl$dmd_type == "AMP"],
+    build_amp_uom_sql
+  )
+
+  cl$uom <- NA_character_
+  vmp_rows <- cl$dmd_type == "VMP"
+  amp_rows <- cl$dmd_type == "AMP"
+  cl$uom[vmp_rows] <- vmp_uom$uom[match(cl$lookup_id[vmp_rows], vmp_uom$lookup_id)]
+  cl$uom[amp_rows] <- amp_uom$uom[match(cl$code[amp_rows], amp_uom$lookup_id)]
+
+  out <- cl[, c("code", "term", "dmd_id", "dmd_type", "uom")]
+  write_csv(out, output_path)
+  invisible(out)
+}
+
+# save codelists with uom column
+save_codelist(
+  "local_codelists/user-emprestige-all-statins-uom.csv",
+  "codelists/user-emprestige-all-statins-dmd.csv"
+)
+save_codelist(
+  "local_codelists/user-emprestige-codeine-for-pain-uom.csv",
+  "codelists/user-emprestige-codeine-for-pain-dmd.csv"
+)
