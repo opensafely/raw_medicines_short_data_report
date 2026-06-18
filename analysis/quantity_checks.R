6c035b7d5987a6812bb64820f90abc7d26900ca5
Em Prestige <69582748+emprestige@users.noreply.github.com>
2026-06-18 13:52:40 +0000

    add quantity checks

---
+++ analysis/quantity_checks.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(here)
+library(arrow)
+library(readr)
+
+# import dataset
+df <- read_feather(here::here("output", "dataset.arrow")) %>% 
+  select(c(patient_id, statin_quantity, statin_code, statin_quantity_rep, statin_code_rep,
+           opioids_quantity, opioids_code, opioids_quantity_rep, opioids_code_rep))
+
+# import codelists with uom
+uom_statin <- read_csv(
+  here::here("local_codelists", "user-emprestige-all-statins-uom.csv"),
+  col_types = cols(code = col_character(),
+                   term = col_character(),
+                   dmd_id = col_character(),
+                   dmd_type = col_character(),
+                   uom = col_character())
+  )
+uom_opioids <- read_csv(
+  here::here("local_codelists", "opensafely-high-dose-long-acting-opioids-openprescribing-dmd-uom.csv"),
+  col_types = cols(code = col_character(),
+                   term = col_character(),
+                   dmd_id = col_character(),
+                   dmd_type = col_character(),
+                   uom = col_character())
+  )
+
+# attach term + uom for each code column
+df_with_uom <- df %>%
+  left_join(
+    uom_statin %>% select(c(code, term, uom)) %>% rename(statin_term = term, statin_uom = uom),
+    by = c("statin_code" = "code")
+  ) %>%
+  left_join(
+    uom_statin %>% select(c(code, term, uom)) %>% rename(statin_term_rep = term, statin_uom_rep = uom),
+    by = c("statin_code_rep" = "code")
+  ) %>%
+  left_join(
+    uom_opioids %>% select(c(code, term, uom)) %>% rename(opioids_term = term, opioids_uom = uom),
+    by = c("opioids_code" = "code")
+  ) %>%
+  left_join(
+    uom_opioids %>% select(c(code, term, uom)) %>% rename(opioids_term_rep = term, opioids_uom_rep = uom),
+    by = c("opioids_code_rep" = "code")
+  )
+
+# skim the data
+capture.output(
+  df_with_uom %>%
+    skimr::skim_without_charts(),
+  file = here::here("output", "quantity_skim.txt"),
+  split = FALSE
+)
